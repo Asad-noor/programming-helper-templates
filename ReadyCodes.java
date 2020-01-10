25115930ee40676d2675e30cf323487948285989
Md. Asaduzzaman Noor <md.noor.asad@gmail.com>
2020-01-11 02:06:06 +0600

    Add ready codes for java programming help.

---
+++ ReadyCodes.java
@@ -0,0 +1,47 @@
+class ReadyCodes{
+    //is even by bitwise
+    int isEvenInt(int n) {
+        return (n & 1);
+    }
+    bollean isEven() {
+        return isEvenInt(5) == 0;
+    }
+    
+    //String to int
+    void stringToInt() {
+        String myString = "1234";
+        int foo = Integer.parseInt(myString);
+    }
+    
+    //int to String
+    void intToString() {
+        int a = 1234; 
+        String str1 = Integer.toString(a);
+    }
+    
+    //char to int
+    void charToInt() {
+        char c='1';  
+        int a = Character.getNumericValue(c);  
+    }
+    
+    //substring example
+    void substringExample() {
+        String s1="javatpoint";  
+        System.out.println(s1.substring(2,4));//returns va  
+        System.out.println(s1.substring(2));//returns vatpoint  
+    }
+    
+    //String indexOf where partialString = "fl"
+    void getIndexOf(String partialString) {
+        String mainString = "flower";
+        int i = mainString.indexOf(partialString);
+        //will return 0
+    }
+    
+    //sort array
+    void sortExample(int A[]) {
+        Arrays.sort(A);
+        //now the A array is sorted
+    }
+}
